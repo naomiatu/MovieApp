747910650b74a8c351ef79817499b311e2b087f2
naomi <g00470124@atu.ie>
2025-11-03 18:43:16 +0000

    Updated xcode so rider can build project

--- MovieApp/Platforms/iOS/AppDelegate.cs
+++ MovieApp/Platforms/iOS/AppDelegate.cs
@@ -5,5 +5,5 @@ namespace MovieApp;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
