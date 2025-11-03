c334d43dfd8f83f7434814ddf000de7ea6b4a51c
naomi <g00470124@atu.ie>
2025-11-03 18:30:04 +0000

    Initial commit

---
+++ MovieApp/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MovieApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MovieApp.Pages")]
